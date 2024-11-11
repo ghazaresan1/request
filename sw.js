397ceb87fd41f8e041a503fcd2b1245721f6ffaf
ghazaresan1 <itman888@yahoo.com>
2024-11-11 13:49:27 +0330

    Update sw.js

--- sw.js
+++ sw.js
@@ -5,3 +5,12 @@ self.addEventListener('install', (event) => {
 self.addEventListener('activate', (event) => {
     clients.claim();
 });
+
+self.addEventListener('message', (event) => {
+    if (event.data === 'requestNotification') {
+        self.registration.showNotification('Portal.Ghazaresan.com', {
+            body: 'Enabling notifications...',
+            icon: 'icon-192x192.png'
+        });
+    }
+});
